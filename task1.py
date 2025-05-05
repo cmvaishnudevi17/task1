94870c6c3b024174995a1d945ea6307f79335b8d
cmvaishnudevi17 <cmvaishnudevi@gmail.com>
2025-05-05 20:43:19 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,40 @@
+Task 1: Format function with two arguments
+
+def format numbers (num, char):
+formatted string "First argument: (0). Second argument: [1]".fo format faun, ch print("\ni. Formatted strings", formatted_string) print("Representation used: positional formatting with str.format at() method") char)
+
+Cormat numbers (145, o")
+
+Task 21 Calculate pond area and water volume
+
+cadias 84
+
+13.14
+
+Calculate area
+
+bond area pi radius 2
+
+rint("\n2. Area of the pond:", pond area, "square meters")
+
+ater per sqm 1.4
+
+Bonus: Calculate total water (1.4 litera per aquare meter)
+
+otal water pond area water_per_sqm
+
+rint ("Total water in the ponds", int (total water), "liters (without decinal ala)")
+
+Tank 3t Calculate speed
+
+intance 490 meters
+
+ine minutes 7
+
+Convert time to seconds ine seconda tine minutes 60
+
+Calculate speed
+
+peed distance/time seconda
+
+rint("\n3. Speed:", int (speed), "neters per second (without decimals)")
